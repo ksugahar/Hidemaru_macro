ecaa1d5c9faa0577b2df336cda2d38e7b0080278
ksugahar <ksugahar@kindai.ac.jp>
2024-05-24 13:27:17 +0200

    update

---
+++ git_upload.sh
@@ -0,0 +1,7 @@
+#git init
+#git remote add origin git@github.com:ksugahar/Hidemaru_macro.git
+git add RunScript.mac
+git add RunScript.exe
+git add RunMatlab.exe
+git add 秀丸設定.reg
+git add git_upload.sh
