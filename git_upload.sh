a724a8c768a1d21986ec9af9b4cba1c3f27f0211
ksugahar <ksugahar@kindai.ac.jp>
2024-05-25 06:05:56 +0200

    update

--- git_upload.sh
+++ git_upload.sh
@@ -1,7 +1,17 @@
+#! /bin/bash
 #git init
 #git remote add origin git@github.com:ksugahar/Hidemaru_macro.git
-git add RunScript.mac
-git add RunScript.exe
-git add RunMatlab.exe
-git add 秀丸設定.reg
+#git pull origin main
+
+#git add RunScript.mac
+#git add RunScript.exe
+#git add RunMatlab.exe
+#git add Latex.hilight
+#git add Matlab.hilight
+#git add Python.hilight
+#git add UWSC.hilight
+#git add 秀丸設定.reg
 git add git_upload.sh
+
+git commit -m "update"
+git push origin main
